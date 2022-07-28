c63da6e30ed11bc2ac4260d60ea2e308870f9e35
LCagmac <42069097+LCagmac@users.noreply.github.com>
2022-07-28 18:08:20 -0400

    Delete kaleidoscopelivecodeI.py

--- kaleidoscopelivecodeI.py
+++
@@ -1,7 +0,0 @@
-while True:  # making a loop
-    try:  # used try so that if user pressed other than the given key error will not be shown
-        if keyboard.is_pressed('a'):  # if key 'q' is pressed
-            neoscore.show(refresh_func_new)
-            break  # finishing the loop
-    except:
-        break  # if user pressed a key other than the given key the loop
